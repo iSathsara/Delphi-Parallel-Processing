89842ecf357b9c9a1d7b578b0666ece23e021326
isuru_s <rmx.sas@gmail.com>
2022-11-30 12:26:05 +0530

    Add thread-safe reading

--- Parallel_for Interlocked/uParallelFor_Interlocked.pas
+++ Parallel_for Interlocked/uParallelFor_Interlocked.pas
@@ -23,6 +23,7 @@ type
   private
     { Private declarations }
     FTotal: Int64;
+    FThreadSafe: Boolean;
   public
     { Public declarations }
   end;
@@ -40,6 +41,7 @@ uses
 procedure TForm5.btnNoThreadSafeClick(Sender: TObject);
 begin
   FTotal:= 0;
+  FThreadSafe:= False;
   TParallel.For(0, 10000, procedure(index: integer) Begin
                             Sleep(2);
                             FTotal:= FTotal + Index;
@@ -50,6 +52,7 @@ procedure TForm5.btnThreadSafeClick(Sender: TObject);
 begin
   // FTotal:= 0; is replaced with
   TInterlocked.Exchange(FTotal, 0);
+  FThreadSafe:= True;
 
   TTask.Run(procedure Begin
     TParallel.For(0, 10000, procedure(index: integer) Begin
@@ -63,13 +66,21 @@ end;
 
 procedure TForm5.FormCreate(Sender: TObject);
 begin
+  FThreadSafe:= False;
   Timer.Enabled:= True;
   FloatAnimation.Enabled:= True;
 end;
 
 procedure TForm5.TimerTimer(Sender: TObject);
+var
+  Value: Int64;
 begin
-  lblResult.Text:= FTotal.ToString;
+
+  if FThreadSafe then Begin
+    Value:= TInterlocked.Read(FTotal);
+    lblResult.Text:= Value.ToString;
+  End else
+    lblResult.Text:= FTotal.ToString;
 end;
 
 end.
