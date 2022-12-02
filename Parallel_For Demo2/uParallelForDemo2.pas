bcb21a58dc4ab599ec59dfe86cfefa759d20208e
isuru_s <rmx.sas@gmail.com>
2022-12-02 16:44:12 +0530

    create shapes dynamically

--- Parallel_For Demo2/uParallelForDemo2.pas
+++ Parallel_For Demo2/uParallelForDemo2.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Edit, FMX.Controls.Presentation, FMX.Layouts;
+  FMX.Edit, FMX.Controls.Presentation, FMX.Layouts, FMX.Objects;
 
 type
   TForm5 = class(TForm)
@@ -14,107 +14,50 @@ type
     Edit1: TEdit;
     Label1: TLabel;
     GridLayout: TGridLayout;
-    Panel1: TPanel;
-    Panel2: TPanel;
-    Panel3: TPanel;
-    Panel4: TPanel;
-    Panel5: TPanel;
-    Panel6: TPanel;
-    Panel7: TPanel;
-    Panel8: TPanel;
-    Panel9: TPanel;
-    Panel10: TPanel;
-    Panel11: TPanel;
-    Panel12: TPanel;
-    Panel13: TPanel;
-    Panel14: TPanel;
-    Panel15: TPanel;
-    Panel16: TPanel;
-    Panel17: TPanel;
-    Panel18: TPanel;
-    Panel19: TPanel;
-    Panel20: TPanel;
-    Panel21: TPanel;
-    Panel22: TPanel;
-    Panel23: TPanel;
-    Panel24: TPanel;
-    Panel25: TPanel;
-    Panel26: TPanel;
-    Panel27: TPanel;
-    Panel28: TPanel;
-    Panel29: TPanel;
-    Panel30: TPanel;
-    Panel31: TPanel;
-    Panel32: TPanel;
-    Panel33: TPanel;
-    Panel34: TPanel;
-    Panel35: TPanel;
-    Panel36: TPanel;
-    Panel37: TPanel;
-    Panel38: TPanel;
-    Panel39: TPanel;
-    Panel40: TPanel;
-    Panel41: TPanel;
-    Panel42: TPanel;
-    Panel43: TPanel;
-    Panel44: TPanel;
-    Panel45: TPanel;
-    Panel46: TPanel;
-    Panel47: TPanel;
-    Panel48: TPanel;
-    Panel49: TPanel;
-    Panel50: TPanel;
-    Panel51: TPanel;
-    Panel52: TPanel;
-    Panel53: TPanel;
-    Panel54: TPanel;
-    Panel55: TPanel;
-    Panel56: TPanel;
-    Panel57: TPanel;
-    Panel58: TPanel;
-    Panel59: TPanel;
-    Panel60: TPanel;
-    Panel61: TPanel;
-    Panel62: TPanel;
-    Panel63: TPanel;
-    Panel64: TPanel;
-    Panel65: TPanel;
-    Panel66: TPanel;
-    Panel67: TPanel;
-    Panel68: TPanel;
-    Panel69: TPanel;
-    Panel70: TPanel;
-    Panel71: TPanel;
-    Panel72: TPanel;
-    Panel73: TPanel;
-    Panel74: TPanel;
-    Panel75: TPanel;
-    Panel76: TPanel;
-    Panel77: TPanel;
-    Panel78: TPanel;
-    Panel79: TPanel;
-    Panel80: TPanel;
-    Panel81: TPanel;
-    Panel82: TPanel;
-    Panel83: TPanel;
-    Panel84: TPanel;
-    Panel85: TPanel;
-    Panel86: TPanel;
-    Panel87: TPanel;
-    Panel88: TPanel;
+    lblItemCount: TLabel;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
+    FItemCount: Integer;
+
+    function CreateShape: TRectangle;
   public
     { Public declarations }
   end;
 
 var
-  Form5: TForm5;
+  Form5 : TForm5;
+  Layout: TGridLayout;
 
 implementation
 
 {$R *.fmx}
 
 
+
+function TForm5.CreateShape: TRectangle;
+var MyRectangle: TRectangle;
+begin
+                                 // "on" which component do you want to create the dynamic object
+  MyRectangle:= TRectangle.Create(Layout);
+  MyRectangle.Visible:=True;
+
+  Result:= MyRectangle;
+end;
+
+procedure TForm5.FormCreate(Sender: TObject);
+Var
+  AObject: TFmxObject;
+  I : Integer;
+
+begin
+  for I := 0 to 87 do Begin
+    AObject:= Self.CreateShape;
+    AObject.Name:='Shape'+I.ToString;
+    GridLayout.AddObject(AObject);
+  End;
+  FItemCount:= GridLayout.ChildrenCount;
+  lblItemCount.Text:= 'Rectangles : '+FItemCount.ToString;
+end;
 
 end.
