c1a3c3d6bef9f9e50deb08762de58a98c9cf8de1
isuru_s <rmx.sas@gmail.com>
2022-12-05 13:48:49 +0530

    paint in normal for loop; ok

--- Parallel_For Demo2/uParallelForDemo2.pas
+++ Parallel_For Demo2/uParallelForDemo2.pas
@@ -11,18 +11,22 @@ type
   TForm5 = class(TForm)
     btnNormalForLoop: TButton;
     btnParallelForLoop: TButton;
-    Edit1: TEdit;
+    edtStride: TEdit;
     Label1: TLabel;
     GridLayout: TGridLayout;
     lblItemCount: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure btnNormalForLoopClick(Sender: TObject);
+    procedure btnParallelForLoopClick(Sender: TObject);
   private
     { Private declarations }
     FItemCount : Integer;
     FItemList  : array of TFmxObject;
 
     function CreateShape: TRectangle;
+
+    procedure Clear;
+    procedure NormalPaint(var index: Integer);
   public
     { Public declarations }
   end;
@@ -41,18 +45,59 @@ uses
 
 {$R *.fmx}
 
-procedure TForm5.btnNormalForLoopClick(Sender: TObject);
+// clear shapes
+procedure TForm5.Clear;
 var
   I : Integer;
   ARectangle: TRectangle;
 begin
-
-  // paint shape by shape
-  for I := 0 to Length(FItemList)-1 do Begin
-                 // typecast
+  for I:= Low(FItemList)  to High(FItemList) do Begin
     ARectangle:= TRectangle(FItemList[I]);
-    ARectangle.Fill.Color:= TAlphaColors.Blueviolet;
+    ARectangle.Fill:= TBrush.Create(TBrushKind.Solid, $FFE0E0E0);
   End;
+end;
+
+// Paint in formal for loop
+procedure TForm5.NormalPaint(var index: Integer);
+var
+  I: Integer;
+  ARectangle: TRectangle;
+begin
+  if FItemList[index] <> nil then Begin
+    ARectangle:= TRectangle(FItemList[index]);
+    ARectangle.Fill.Color:= TAlphaColors.Blueviolet;
+  End else
+    raise Exception.Create('Requested item is null');
+End;
+
+procedure TForm5.btnNormalForLoopClick(Sender: TObject);
+var
+  I: Integer;
+  lFrom, lTo: Integer;
+begin
+  Clear;
+  TTask.Run(procedure
+            var
+              index: Integer;
+            Begin
+              for index := Low(FItemList) to High(FItemList) do begin
+                NormalPaint(index);
+                Sleep(100);
+              end;
+            End);
+end;
+
+procedure TForm5.btnParallelForLoopClick(Sender: TObject);
+var ARectangle: TRectangle;
+    I: Integer;
+
+begin
+  Clear;
+  TTask.Run(procedure
+            Begin
+
+            End);
+
 end;
 
 function TForm5.CreateShape: TRectangle;
